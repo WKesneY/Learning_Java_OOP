c1abb9bdefec62ae91ede821d30bab0d931ad75f
WKesneY <wildneysousa25@gmail.com>
2023-09-01 18:21:44 -0300

    LISTA 01 - Questões FOR

---
+++ LISTAS DE EXERCÍCIOS/Lista02 - Estrutura FOR/Media.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int vet[] = new int[10];
+        int recebeNum;
+
+        System.out.println("--------------INFORME DEZ VALORES--------------");
+
+        for(int i = 0; i < vet.length; i++){
+            System.out.printf("\nINFORME UM NUMERO: ");
+            recebeNum = scanner.nextInt();
+
+            vet[i] = recebeNum;
+            
+        }
+
+        for(int i = 0; i < vet.length; i++){
+            System.out.printf("[%d] ", vet[i]);
+        }
+
+        int soma = 0;
+        for(int i = 0; i < vet.length; soma+=vet[i], i++);
+
+        double media = soma / vet.length;
+        System.out.println("\n\n-> A MEDIA É: " + media + "<-");
+
+    }
+    
+}
---
+++ LISTAS DE EXERCÍCIOS/Lista02 - Estrutura FOR/Notas.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Notas {
+    public static void main(String[] args) {
+        int quantAlunos;
+        double mediaTurmas;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("------------------ MEDIA TURMA ------------------");
+        System.out.printf("\n\nInforme a quantidade de alunos da turma:\t");
+        quantAlunos = scanner.nextInt();
+
+        
+
+
+
+
+    }
+    
+}
