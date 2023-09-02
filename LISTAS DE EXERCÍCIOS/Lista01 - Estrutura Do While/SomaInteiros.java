cee8f1db78ce262d4582f5d71dff0fff8ae3cd1c
WKesneY <wildneysousa25@gmail.com>
2023-09-02 16:47:36 -0300

    Lista 01 - While - Questão 12

--- LISTAS DE EXERCÍCIOS/Lista01 - Estrutura Do While/SomaInteiros.java
+++ LISTAS DE EXERCÍCIOS/Lista01 - Estrutura Do While/SomaInteiros.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 public class SomaInteiros {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("----------- SOMA DE INTEIROS -----------");
+        System.out.print("Infome um número:\t");
+        int num = scanner.nextInt();
+        scanner.close();
+        int aux = 1, soma = 0;
+
+        do{
+            soma += aux;
+            aux++;
+        }while(aux <= num);
+
+        System.out.println("\n-> [SOMA TOTAL = " + soma + "] <-");
+
     }
     
 }
