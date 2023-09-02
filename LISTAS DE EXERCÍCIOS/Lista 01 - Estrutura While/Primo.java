999ed858ee0eac92509ae66e04611704deb110fb
WKesneY <wildneysousa25@gmail.com>
2023-09-02 16:05:02 -0300

    Lista01 - While - Questões

--- LISTAS DE EXERCÍCIOS/Lista 01 - Estrutura While/Primo.java
+++ LISTAS DE EXERCÍCIOS/Lista 01 - Estrutura While/Primo.java
@@ -2,7 +2,30 @@ import java.util.*;
 
 public class Primo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+
+        System.out.println("---------------- CHECAR SE É PRIMO ----------------");
+
+        while(true){
+        int aux = 0;
+        System.out.print("\nInforme um número:");
+        num = scanner.nextInt();
         
+        for(int i = 2; i <= num / 2; i++){
+            if(num % i == 0){
+                aux++;
+                break;
+            }
+        }
+
+        if(aux == 0){
+            System.out.println("-> NUMERO PRIMO <-");
+            break;
+        }else System.out.println("-> NUMERO NAO PRIMO <-");
+
+        }
+                
+        scanner.close();
     }
-    
 }
