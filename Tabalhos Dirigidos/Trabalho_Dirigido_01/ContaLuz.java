b806b66e1fa6a4d3d60ba666b2214feda6512142
WKesneY <wildneysousa25@gmail.com>
2023-09-07 01:02:18 -0300

    Organizando pastas

--- Tabalhos Dirigidos/Trabalho_Dirigido_01/ContaLuz.java
+++ Tabalhos Dirigidos/Trabalho_Dirigido_01/ContaLuz.java
@@ -1,3 +1,26 @@
 public class ContaLuz{
+    String codBarras;
+    double gastoKw;
+    double valorConta;
+
+    void setCodBarras(String codigo){
+        this.codBarras = codigo;
+    }
+    void setGastoKw(double gasto){
+        this.gastoKw = gasto;
+    }
+    void setvalorConta(double valor){
+        this.valorConta = valor;
+    }
+
+    String getCodBarras(){
+        return codBarras;
+    }
+    double getGastoKw(){
+        return gastoKw;
+    }
+    double getValorConta(){
+        return valorConta;
+    }
     
 }
