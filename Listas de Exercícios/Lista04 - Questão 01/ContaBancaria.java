59dc699047e2b406ef57fc503cd3db1c9936d9d1
WKesneY <wildneysousa25@gmail.com>
2023-10-29 10:16:11 -0300

    Classe ContaPoupanca implementada

--- Listas de Exercícios/Lista04 - Questão 01/ContaBancaria.java
+++ Listas de Exercícios/Lista04 - Questão 01/ContaBancaria.java
@@ -1,6 +1,6 @@
 public abstract class ContaBancaria{
-    private String numeroConta;
-    private double saldo;
+    protected String numeroConta;
+    protected double saldo;
 
     public ContaBancaria(String numeroConta, double saldo){
         this.numeroConta = numeroConta;
@@ -17,18 +17,19 @@ public abstract class ContaBancaria{
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    public abstract double getSal6do();
+    public abstract double getSaldo();
 
     //Métodos Concretos:
     public void depositar (double valor){
         saldo += valor;
     }
 
-    public void sacar (double valor){
-        if((saldo - valor) < 0){
+    public void sacar(double valor) {
+        if (saldo >= valor) {
             saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente, informe um valor válido.");
         }
-        System.out.println("Saldo insuficiente, informe um valor válido.");
     }
 
 
--- Listas de Exercícios/Lista04 - Questão 01/ContaPoupanca.java
+++ Listas de Exercícios/Lista04 - Questão 01/ContaPoupanca.java
@@ -1,3 +1,13 @@
 public class ContaPoupanca extends ContaBancaria{
-    
+    private double taxaRendimento; 
+
+    public ContaPoupanca(String numeroConta, double saldo, double taxaRendimento){
+            super(numeroConta, saldo);
+            this.taxaRendimento = taxaRendimento;
+        }
+
+    public double getSaldo(){
+        return getSaldo() + (getSaldo() * (taxaRendimento / 100));
+    }
+
 }
