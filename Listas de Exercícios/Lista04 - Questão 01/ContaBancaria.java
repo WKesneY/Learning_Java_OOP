21bbad708392f23ffe3e74b5211946e4fcb0273b
WKesneY <wildneysousa25@gmail.com>
2023-10-29 09:55:43 -0300

    Implementando questão 1, Lista 04

--- Listas de Exercícios/Lista04 - Questão 01/ContaBancaria.java
+++ Listas de Exercícios/Lista04 - Questão 01/ContaBancaria.java
@@ -1,21 +1,23 @@
 public abstract class ContaBancaria{
-    private int contaBancaria;
+    private String numeroConta;
     private double saldo;
 
-    //Getters and Setters:
-    public void setContaBancaria(int contaBancaria) {
-        this.contaBancaria = contaBancaria;
+    public ContaBancaria(String numeroConta, double saldo){
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
     }
-    public int getContaBancaria() {
-        return contaBancaria;
+    //Getters and Setters:
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public String getNumeroConta() {
+        return numeroConta;
     }
     
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    public double getSaldo() {
-        return saldo;
-    }
+    public abstract double getSal6do();
 
     //Métodos Concretos:
     public void depositar (double valor){
@@ -23,9 +25,11 @@ public abstract class ContaBancaria{
     }
 
     public void sacar (double valor){
-        if(saldo - valor < 0){
+        if((saldo - valor) < 0){
             saldo -= valor;
         }
         System.out.println("Saldo insuficiente, informe um valor válido.");
     }
+
+
 }
